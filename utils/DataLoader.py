339ef6698ac5be847c8ea75f3859da3c99cbdc7e
QI-Joe <jqi221086@gmail.com>
2025-05-16 21:41:47 +0800

    Loading format update

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -98,26 +98,20 @@ def to_TPPR_Data(graph: Temporal_Dataloader) -> Data:
     hash_table: dict[int, int] = {idx: node for idx, node in zip(*hash_dataframe)}
 
     edge_feat, node_feat = graph.edge_pos, graph.node_pos
-    TPPR_data = Data(sources= src, destinations=dest, timestamps=timestamp, edge_idxs = edge_idx, \
+    TPPR_data = Data(src_node_ids= src, dst_node_ids=dest, node_interact_times=timestamp, edge_ids = edge_idx, \
                      labels=labels, hash_table=hash_table, node_feat=node_feat, edge_feat=edge_feat)
 
     return TPPR_data
 
-def quantile_(threshold: float, timestamps: torch.Tensor) -> tuple[torch.Tensor]:
-    full_length = timestamps.shape[0]
-    val_idx = int(threshold*full_length)
+def span_time_quantile(threshold: float, tsp: np.ndarray, dataset: str):
+    val_time = np.quantile(tsp, threshold)
+    
+    if dataset in ["dblp", "tmall"]:
+        spans, span_freq = np.unique(tsp, return_counts=True)
+        if val_time == spans[-1]: val_time = spans[int(spans.shape[0]*threshold)]
+    return val_time
 
-    if not isinstance(timestamps, torch.Tensor):
-        timestamps = torch.from_numpy(timestamps)
-    train_mask = torch.zeros_like(timestamps, dtype=bool)
-    train_mask[:val_idx] = True
-
-    val_mask = torch.zeros_like(timestamps, dtype=bool)
-    val_mask[val_idx:] = True
-
-    return train_mask, val_mask
-
-def get_link_prediction_data(dataset_name: str, val_ratio: float, test_ratio: float):
+def get_link_prediction_data(dataset_name: str, snapshot: int, val_ratio: float = 0.8, test_ratio: float=0.0):
     """
     generate data for link prediction task (inductive & transductive settings)
     :param dataset_name: str, dataset name
@@ -130,11 +124,10 @@ def get_link_prediction_data(dataset_name: str, val_ratio: float, test_ratio: fl
     # graph_df = pd.read_csv('./processed_data/{}/ml_{}.csv'.format(dataset_name, dataset_name))
     # edge_raw_features = np.load('./processed_data/{}/ml_{}.npy'.format(dataset_name, dataset_name))
     # node_raw_features = np.load('./processed_data/{}/ml_{}_node.npy'.format(dataset_name, dataset_name))
-    snapshot, view = 10, 8
+    view = snapshot - 2
 
     graph, idx_list = data_load(dataset=dataset_name, emb_size = 64)
-    edge_raw_features = graph.edge_pos
-    node_raw_features = graph.node_pos
+    node_raw_features, edge_raw_features = graph.pos
 
     NODE_FEAT_DIM = EDGE_FEAT_DIM = 172
     assert NODE_FEAT_DIM >= node_raw_features.shape[1], f'Node feature dimension in dataset {dataset_name} is bigger than {NODE_FEAT_DIM}!'
@@ -147,102 +140,116 @@ def get_link_prediction_data(dataset_name: str, val_ratio: float, test_ratio: fl
         edge_zero_padding = np.zeros((edge_raw_features.shape[0], EDGE_FEAT_DIM - edge_raw_features.shape[1]))
         edge_raw_features = np.concatenate([edge_raw_features, edge_zero_padding], axis=1)
     
-    graph.edge_pos, graph.node_pos = edge_raw_features, node_raw_features
+    graph.pos = node_raw_features, edge_raw_features
 
     assert NODE_FEAT_DIM == node_raw_features.shape[1] and EDGE_FEAT_DIM == edge_raw_features.shape[1], 'Unaligned feature dimensions after feature padding!'
-    graph_list = Temporal_Splitting(graph).temporal_splitting(time_mode="view", snapshot=snapshot, view = view)
+    graph_list = Temporal_Splitting(graph).temporal_splitting(time_mode="view", snapshot=snapshot, views = view)
     
-    # number_of_views = len(graph_list)
-    # for idx in range(full_data-1):
-    temporal_graph = graph_list[0]
-    # get the timestamp of validate and test set
-    full_data = to_TPPR_Data(temporal_graph)
-    src_node_ids = full_data.src_node_ids.astype(np.longlong)
-    dst_node_ids = full_data.dst_node_ids.astype(np.longlong)
-    node_interact_times = full_data.node_interact_times.astype(np.float64)
-    edge_ids = full_data.edge_ids.astype(np.longlong)
-    labels = full_data.labels.values
-    
-    train_mask, val_mask = quantile_(0.80, timestamps=node_interact_times)
-    val_time = np.quantile(node_interact_times, 0.85)
-    
-    if dataset_name in ["dblp", "tmall"]:
-        spans, span_freq = np.unique(node_interact_times, return_counts=True)
-        if val_time == spans[-1]: val_time = spans[int(spans.shape[0]*0.8)]
+    assert len(graph_list) == view, f"the number of views should be {view}, but got {len(graph_list)}"
+    Data_list = list()
+    for idx in range(view-1):
+        temporal_graph = graph_list[idx]
+        # get the timestamp of validate and test set
+        full_data = to_TPPR_Data(temporal_graph)
+        src_node_ids = full_data.src_node_ids.astype(np.longlong)
+        dst_node_ids = full_data.dst_node_ids.astype(np.longlong)
+        node_interact_times = full_data.node_interact_times.astype(np.float64)
+        edge_ids = full_data.edge_ids.astype(np.longlong)
+        labels = full_data.labels
+        
+        val_time = span_time_quantile(threshold=0.8, tsp=node_interact_times, dataset=dataset_name)
 
-    # the setting of seed follows previous works
-    random.seed(2020)
 
-    # union to get node set
-    node_set = set(src_node_ids) | set(dst_node_ids)
-    num_total_unique_node_ids = len(node_set)
+        # the setting of seed follows previous works
+        random.seed(2020)
 
-    # compute nodes which appear at test time
-    t1_temporal = graph_list[0+1]
-    
-    test_node_set = set(src_node_ids[node_interact_times > val_time]).union(set(dst_node_ids[node_interact_times > val_time]))
-    # sample nodes which we keep as new nodes (to test inductiveness), so then we have to remove all their edges from training
-    new_test_node_set = set(random.sample(test_node_set, int(0.1 * num_total_unique_node_ids)))
+        # union to get node set
+        node_set = set(src_node_ids) | set(dst_node_ids)
+        num_total_unique_node_ids = len(node_set)
 
-    # mask for each source and destination to denote whether they are new test nodes
-    new_test_source_mask = graph_df.u.map(lambda x: x in new_test_node_set).values
-    new_test_destination_mask = graph_df.i.map(lambda x: x in new_test_node_set).values
+        # compute nodes which appear at test time
+        t1_temporal: Temporal_Dataloader = graph_list[idx+1]
+        t1_full_data: Data = to_TPPR_Data(t1_temporal)
+        t1_node_set = set(t1_full_data.src_node_ids) | set(t1_full_data.dst_node_ids)
+        t1_num_unique_node_ids = len(t1_node_set)
+        t_hash_table, t1_hash_table = full_data.hash_table, t1_full_data.hash_table
+        
+        """
+        Basically, test_node_set should be valid node set consdiering its temporal features, lets make a row:
+        new val test set = set(random.sample(val_ndoe_set, int(0.1 * val_node_set))
+        """
+        val_node_set = set(src_node_ids[node_interact_times > val_time]).union(set(dst_node_ids[node_interact_times > val_time]))
+        new_val_node_set = set(random.sample(sorted(val_node_set), int(0.05 * num_total_unique_node_ids)))
+        
+        new_val_source_mask = np.isin(src_node_ids, sorted(new_val_node_set))
+        new_val_destination_mask = np.isin(dst_node_ids, sorted(new_val_node_set))
+        
+        observed_edge_mask = np.logical_and(~new_val_source_mask, ~new_val_destination_mask)
+        # for train data, we keep edges happening before the validation time which do not involve any new node, used for inductiveness
+        train_mask = np.logical_and(node_interact_times <= val_time, observed_edge_mask)
+        # train_mask = node_interact_times<=val_time
 
-    # mask, which is true for edges with both destination and source not being new test nodes (because we want to remove all edges involving any new test node)
-    observed_edges_mask = np.logical_and(~new_test_source_mask, ~new_test_destination_mask)
+        train_data = Data(src_node_ids=src_node_ids[train_mask], dst_node_ids=dst_node_ids[train_mask],
+                        node_interact_times=node_interact_times[train_mask],
+                        edge_ids=edge_ids[train_mask], labels=labels)
 
-    # for train data, we keep edges happening before the validation time which do not involve any new node, used for inductiveness
-    train_mask = np.logical_and(node_interact_times <= val_time, observed_edges_mask)
+        # define the new nodes sets for testing inductiveness of the model
+        train_node_set = set(train_data.src_node_ids).union(train_data.dst_node_ids)
+        # assert len(train_node_set & new_val_node_set) == 0
+        # new nodes that are not in the training set
+        new_node_set = node_set - train_node_set # key points 1
 
-    train_data = Data(src_node_ids=src_node_ids[train_mask], dst_node_ids=dst_node_ids[train_mask],
-                      node_interact_times=node_interact_times[train_mask],
-                      edge_ids=edge_ids[train_mask], labels=labels[train_mask])
+        val_mask = node_interact_times > val_time
 
-    # define the new nodes sets for testing inductiveness of the model
-    train_node_set = set(train_data.src_node_ids).union(train_data.dst_node_ids)
-    assert len(train_node_set & new_test_node_set) == 0
-    # new nodes that are not in the training set
-    new_node_set = node_set - train_node_set
+        # new edges with new nodes in the val and test set (for inductive evaluation)
+        edge_contains_new_node_mask = np.array([(src_node_id in new_node_set or dst_node_id in new_node_set)
+                                                for src_node_id, dst_node_id in zip(src_node_ids, dst_node_ids)])
+        new_node_val_mask = np.logical_and(val_mask, edge_contains_new_node_mask)
 
-    val_mask = np.logical_and(node_interact_times <= test_time, node_interact_times > val_time)
-    test_mask = node_interact_times > test_time
+        # validation and test data
+        val_data = Data(src_node_ids=src_node_ids[val_mask], dst_node_ids=dst_node_ids[val_mask],
+                        node_interact_times=node_interact_times[val_mask], edge_ids=edge_ids[val_mask], labels=labels)
 
-    # new edges with new nodes in the val and test set (for inductive evaluation)
-    edge_contains_new_node_mask = np.array([(src_node_id in new_node_set or dst_node_id in new_node_set)
-                                            for src_node_id, dst_node_id in zip(src_node_ids, dst_node_ids)])
-    new_node_val_mask = np.logical_and(val_mask, edge_contains_new_node_mask)
-    new_node_test_mask = np.logical_and(test_mask, edge_contains_new_node_mask)
+        test_data = t1_full_data
 
-    # validation and test data
-    val_data = Data(src_node_ids=src_node_ids[val_mask], dst_node_ids=dst_node_ids[val_mask],
-                    node_interact_times=node_interact_times[val_mask], edge_ids=edge_ids[val_mask], labels=labels[val_mask])
+        # validation and test with edges that at least has one new node (not in training set)
+        new_node_val_data = Data(src_node_ids=src_node_ids[new_node_val_mask], dst_node_ids=dst_node_ids[new_node_val_mask],
+                                node_interact_times=node_interact_times[new_node_val_mask],
+                                edge_ids=edge_ids[new_node_val_mask], labels=labels)
+        """
+        try to not resort the new node set, see whether it works
+        """
+        # t_node_match, t1_node_match = np.vectorize(t1_hash_table.get), np.vectorize(t_hash_table.get)
+        # t_node_original, t1_node_original = t_node_match(train_node_set), t1_node_match(t1_node_match)
+        
+        # t1_new_node_set = t1_node_original - t_node_original # key points 2
+        # reverse_hash_table = {v: k for k, v in t1_hash_table.items()}
+        # reverse_t1_new_node_set = np.vectorize(reverse_hash_table.get)(t1_new_node_set)
+        t1_new_node_set = t1_node_set - train_node_set # key points 2.1
+        t1_edge_contains_new_node_mask = np.array([(src_node_id in t1_new_node_set or dst_node_id in t1_new_node_set)
+                                                    for src_node_id, dst_node_id in zip(t1_full_data.src_node_ids, t1_full_data.dst_node_ids)])
+        new_node_test_mask = np.logical_and(t1_full_data.node_interact_times, t1_edge_contains_new_node_mask)  
+        
+        new_node_test_data = Data(src_node_ids=test_data.src_node_ids[new_node_test_mask], dst_node_ids=test_data.dst_node_ids[new_node_test_mask],
+                                node_interact_times=test_data.node_interact_times[new_node_test_mask],
+                                edge_ids=test_data.edge_ids[new_node_test_mask], labels=test_data.labels)
+        
+        Data_list.append([full_data, train_data, val_data, test_data, new_node_val_data, new_node_test_data])
 
-    test_data = Data(src_node_ids=src_node_ids[test_mask], dst_node_ids=dst_node_ids[test_mask],
-                     node_interact_times=node_interact_times[test_mask], edge_ids=edge_ids[test_mask], labels=labels[test_mask])
+        print("The dataset has {} interactions, involving {} different nodes".format(full_data.num_interactions, full_data.num_unique_nodes))
+        print("The training dataset has {} interactions, involving {} different nodes, with ratio of {:.4f}".format(
+            train_data.num_interactions, train_data.num_unique_nodes, train_data.num_interactions/full_data.num_interactions))
+        print("The validation dataset has {} interactions, involving {} different nodes, with ratio of {:.4f}".format(
+            val_data.num_interactions, val_data.num_unique_nodes, val_data.num_interactions / full_data.num_interactions))
+        print("The test dataset has {} interactions, involving {} different nodes".format(
+            test_data.num_interactions, test_data.num_unique_nodes))
+        print("The new node validation dataset has {} interactions, involving {} different nodes".format(
+            new_node_val_data.num_interactions, new_node_val_data.num_unique_nodes))
+        print("The new node test dataset has {} interactions, involving {} different nodes".format(
+            new_node_test_data.num_interactions, new_node_test_data.num_unique_nodes))
+        print("{} nodes were used for the inductive testing, i.e. are never seen during training\n\n".format(len(t1_new_node_set)))
 
-    # validation and test with edges that at least has one new node (not in training set)
-    new_node_val_data = Data(src_node_ids=src_node_ids[new_node_val_mask], dst_node_ids=dst_node_ids[new_node_val_mask],
-                             node_interact_times=node_interact_times[new_node_val_mask],
-                             edge_ids=edge_ids[new_node_val_mask], labels=labels[new_node_val_mask])
-
-    new_node_test_data = Data(src_node_ids=src_node_ids[new_node_test_mask], dst_node_ids=dst_node_ids[new_node_test_mask],
-                              node_interact_times=node_interact_times[new_node_test_mask],
-                              edge_ids=edge_ids[new_node_test_mask], labels=labels[new_node_test_mask])
-
-    print("The dataset has {} interactions, involving {} different nodes".format(full_data.num_interactions, full_data.num_unique_nodes))
-    print("The training dataset has {} interactions, involving {} different nodes".format(
-        train_data.num_interactions, train_data.num_unique_nodes))
-    print("The validation dataset has {} interactions, involving {} different nodes".format(
-        val_data.num_interactions, val_data.num_unique_nodes))
-    print("The test dataset has {} interactions, involving {} different nodes".format(
-        test_data.num_interactions, test_data.num_unique_nodes))
-    print("The new node validation dataset has {} interactions, involving {} different nodes".format(
-        new_node_val_data.num_interactions, new_node_val_data.num_unique_nodes))
-    print("The new node test dataset has {} interactions, involving {} different nodes".format(
-        new_node_test_data.num_interactions, new_node_test_data.num_unique_nodes))
-    print("{} nodes were used for the inductive testing, i.e. are never seen during training".format(len(new_test_node_set)))
-
-    return node_raw_features, edge_raw_features, full_data, train_data, val_data, test_data, new_node_val_data, new_node_test_data
+    return node_raw_features, edge_raw_features, Data_list # full_data, train_data, val_data, test_data, new_node_val_data, new_node_test_data
 
 
 def get_node_classification_data(dataset_name: str, val_ratio: float, test_ratio: float):
@@ -298,3 +305,17 @@ def get_node_classification_data(dataset_name: str, val_ratio: float, test_ratio
                      node_interact_times=node_interact_times[test_mask], edge_ids=edge_ids[test_mask], labels=labels[test_mask])
 
     return node_raw_features, edge_raw_features, full_data, train_data, val_data, test_data
+
+def quantile_(threshold: float, timestamps: torch.Tensor) -> tuple[torch.Tensor]:
+    full_length = timestamps.shape[0]
+    val_idx = int(threshold*full_length)
+
+    if not isinstance(timestamps, torch.Tensor):
+        timestamps = torch.from_numpy(timestamps)
+    train_mask = torch.zeros_like(timestamps, dtype=bool)
+    train_mask[:val_idx] = True
+
+    val_mask = torch.zeros_like(timestamps, dtype=bool)
+    val_mask[val_idx:] = True
+
+    return train_mask, val_mask
